97f62f9a8106a871271071739e051f0fe55f3bd1
Dragos Varovici <dvarovici.work@gmail.com>
2018-04-21 13:51:22 -0600

    Draft implementation

---
+++ lib/SergePlugins.pm
@@ -0,0 +1,23 @@
+=head1 NAME
+
+SergePlugins - Plugins for Serge (*String Extraction and Resource Generation Engine*)
+
+=head1 DESCRIPTION
+
+Serge Plugins are plugins for Serge
+
+=head1 COPYRIGHT
+
+Copyright (C) 2018, Dragos Varovici.
+
+=head1 SEE ALSO
+
+L<https://serge.io/>
+
+=cut
+
+package SergePlugins;
+
+our $VERSION = '0.1';
+
+1;
